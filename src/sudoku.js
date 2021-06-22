4a7042bc75aaa42ea32fd1754940917c144d47f9
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-22 11:23:27 -0700

    good fail for checkInt method, it WORKSgit add .
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -6,12 +6,14 @@ this.column = column;
 }
 
 Sudoku.prototype.checkInt = function() {
-  // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // const columnArr = this.column;
-  // const rowArr = this.row;
-  // const bothArr = [...columnArr, ...rowArr];
-  // for (let i =0; i < bothArr.length; i ++) {
-  //   if (!bothArr.includes(numbers[i])){
-  //     return "Nice try!!! NOT ALLOWED";
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const columnArr = this.column;
+  const rowArr = this.row;
+  const bothArr = [...columnArr, ...rowArr];
+  for (let i =0; i < bothArr.length; i ++) {
+    if (!bothArr.includes(numbers[i])){
+      return "Nice try!!! NOT ALLOWED";
     }
+  }
+};
 
