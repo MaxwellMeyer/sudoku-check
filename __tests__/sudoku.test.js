d5f3a647d6c1b5a4ed72227f6f27757a8658d1f7
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-22 11:18:54 -0700

    good fail for checkInt method.
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -3,8 +3,12 @@ import Sudoku from '../src/sudoku.js';
 
 describe('Sudoku', () => {
   test('should create a sudoku object', () => {
-    const sudoku = new Sudoku([1,2,3], [4,5,6])
+    const sudoku = new Sudoku([1,2,3], [4,5,6]);
     expect(sudoku.row).toEqual([1,2,3]);
     expect(sudoku.column).toEqual([4,5,6]);
+  });
+  test('should check if all entered numbers are integers between 1-9', () => {
+    const testSudoku = new Sudoku([2, 4, 6, 7], [3, 5, 7, 44]);
+    expect(testSudoku.checkInt()).toEqual("Nice try!!! NOT ALLOWED");
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,17 @@
+// let sudoku = new Soduku ([],[])
+
 export default function Sudoku(row, column) {
 this.row = row;
 this.column = column;
 }
+
+Sudoku.prototype.checkInt = function() {
+  // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // const columnArr = this.column;
+  // const rowArr = this.row;
+  // const bothArr = [...columnArr, ...rowArr];
+  // for (let i =0; i < bothArr.length; i ++) {
+  //   if (!bothArr.includes(numbers[i])){
+  //     return "Nice try!!! NOT ALLOWED";
+    }
+
